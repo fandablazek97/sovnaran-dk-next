251c1114f19bfc61ea97543f0256c276b9701702
fandablazek97 <blazek127@gmail.com>
2023-08-21 16:26:01 +0200

    analytics added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import Favicon from '@/components/Favicon'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Favicon />
       <Component {...pageProps} />
+      <Analytics mode={'production'} />;
     </>
   )
 }
